afe7e3a838e1cdbc7bf01bef38e6c3707ced6398
Marco Chiloiro <115002514+marco-chiloiro@users.noreply.github.com>
2025-03-05 15:50:04 +0100

    Delete training-and-testing directory

--- training-and-testing/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .functions import create_sgb_chimera_dict, create_chimera_paths, import_seqcov_bzip_file, make_labels, merge_labels
-from .functions import create_mega_dict, check_labels_distribution
-from .functions import DataGenerator, create_data_loaders
-from .functions import create_mega_dict_test, test_2_create_sgb_chim_dict, test_3_create_sgb_chim_dict
-from .fit import train_model, evaluate_model, save_model, lr_scheduler
-from .testing import model_import, plot_training_history, build_results_paths, test, save_training_history, combined_test
